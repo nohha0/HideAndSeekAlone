d2c4e967337038034b9b18d09d090f5f15b9d66e
nohha0 <114228512+nohha0@users.noreply.github.com>
2022-10-12 23:21:26 +0900

    맵 이동 테스트

---
+++ Assets/Scripts/NewStage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewStage : MonoBehaviour
+{
+    public enum NextPositionType
+    {
+        InitPosition,
+        SomePosition,
+    };
+    public NextPositionType nextPositionType;
+
+    public Transform DestinationPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))   
+        {
+            if (nextPositionType == NextPositionType.InitPosition)  
+            {
+                collision.transform.position = Vector3.zero;
+            }
+            else if (nextPositionType == NextPositionType.SomePosition)
+            {
+                collision.transform.position = DestinationPoint.position;
+            }
+            else
+            { }
+
+        }
+    }
+}
