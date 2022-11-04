ede1fbc741ba4335768c9912281c219734a5d575
Shin004 <115400628+Shin004@users.noreply.github.com>
2022-11-04 11:45:41 +0900

    파이어볼 추가
    오류 수정 안된거

---
+++ Assets/Scripts/ForwardFireWave.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardFireWave : MonoBehaviour
+{
+    SpriteRenderer rend;
+    public SpriteRenderer plarerRenderer;  //플레이어 랜더러 가져오기
+    public float speed;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        plarerRenderer = GetComponent<SpriteRenderer>();
+        Invoke("OnDestroy", 2);
+    }
+
+    void Update()
+    {
+        if(transform.rotation.y ==0)
+        {
+            rend.flipX = true;
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            rend.flipX = false;
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+        }
+        
+    }
+    private void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
